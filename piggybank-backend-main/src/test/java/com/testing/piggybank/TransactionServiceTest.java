ccf2c577162e7621a4e9b30b2d9eccaf58d0b0e8
TijnKlabbers <Tijnklabbers@gmail.com>
2024-01-25 22:31:09 +0100

    maak testFilterAndLimitTransactions

--- piggybank-backend-main/src/test/java/com/testing/piggybank/TransactionServiceTest.java
+++ piggybank-backend-main/src/test/java/com/testing/piggybank/TransactionServiceTest.java
@@ -1,9 +1,8 @@
 package com.testing.piggybank;
 import com.testing.piggybank.account.AccountService;
 import com.testing.piggybank.helper.CurrencyConverterService;
-import com.testing.piggybank.model.Account;
-import com.testing.piggybank.model.Currency;
-import com.testing.piggybank.model.Direction;
+import com.testing.piggybank.model.*;
+
 import java.time.LocalDateTime;
 import java.util.Collections;
 
@@ -15,7 +14,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.testing.piggybank.model.Transaction;
 import com.testing.piggybank.transaction.CreateTransactionRequest;
 import com.testing.piggybank.transaction.TransactionRepository;
 import com.testing.piggybank.transaction.TransactionService;
@@ -35,7 +33,9 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+import com.testing.piggybank.model.Transaction;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class TransactionServiceTest {
 
@@ -117,12 +117,70 @@ public class TransactionServiceTest {
         verify(transactionRepository, times(1)).save(any());
     }
 
+    @Test
+    public void testSortDescByDateTime() {
+        Transaction t1 = new Transaction();
+        t1.setDateTime(Instant.parse("2024-01-23T12:30:00Z"));
 
+        Transaction t2 = new Transaction();
+        t2.setDateTime(Instant.parse("2024-01-23T13:45:00Z"));
 
+        int result = TransactionService.sortDescByDateTime(t1, t2);
 
+        assertEquals(1, result);
+    }
 
+    @Test
+    public void testFilterAndLimitTransactions() {
+        long accountId = 1L;
+        Integer limit = 1;
 
+        Account senderAccount = new Account();
+        senderAccount.setId(accountId);
+        senderAccount.setName("Tijntje");
+        senderAccount.setBalance(BigDecimal.valueOf(75));
 
+        Account receiverAccount = new Account();
+        receiverAccount.setId(2L);
+        receiverAccount.setName("Melvin");
+        receiverAccount.setBalance(BigDecimal.valueOf(50));
+
+        List<Transaction> mockedTransactions = new ArrayList<>();
+
+        Transaction tijnsEersteTransactie = new Transaction();
+        tijnsEersteTransactie.setSenderAccount(senderAccount);
+        tijnsEersteTransactie.setReceiverAccount(receiverAccount);
+        tijnsEersteTransactie.setAmount(BigDecimal.TEN);
+        tijnsEersteTransactie.setCurrency(Currency.EURO);
+        tijnsEersteTransactie.setDescription("eerste voorbeeldtransactie");
+        tijnsEersteTransactie.setDateTime(Instant.now().plusMillis(1));
+        tijnsEersteTransactie.setStatus(Status.SUCCESS);
+        mockedTransactions.add(tijnsEersteTransactie);
+
+        Transaction tijnsTweedeTransactie = new Transaction();
+        tijnsTweedeTransactie.setSenderAccount(senderAccount);
+        tijnsTweedeTransactie.setReceiverAccount(receiverAccount);
+        tijnsTweedeTransactie.setAmount(BigDecimal.TEN);
+        tijnsTweedeTransactie.setCurrency(Currency.EURO);
+        tijnsTweedeTransactie.setDescription("tweede voorbeeldtransactie");
+        tijnsTweedeTransactie.setDateTime(Instant.now());
+        tijnsTweedeTransactie.setStatus(Status.SUCCESS);
+        mockedTransactions.add(tijnsTweedeTransactie);
+
+        Transaction tijnsDerdeTransactie = new Transaction();
+        tijnsDerdeTransactie.setSenderAccount(senderAccount);
+        tijnsDerdeTransactie.setReceiverAccount(receiverAccount);
+        tijnsDerdeTransactie.setAmount(BigDecimal.TEN);
+        tijnsDerdeTransactie.setCurrency(Currency.EURO);
+        tijnsDerdeTransactie.setDescription("derde voorbeeldtransactie");
+        tijnsDerdeTransactie.setDateTime(Instant.now());
+        tijnsDerdeTransactie.setStatus(Status.SUCCESS);
+        mockedTransactions.add(tijnsDerdeTransactie);
+
+        List<Transaction> result = transactionService.filterAndLimitTransactions(mockedTransactions, accountId, limit);
+
+        assertEquals(1, result.size());
+    }
 }
 
 
